5abaca9cfc1b621ffd114abc599235051ef93905
Omar Eqbal <eqbalomar@gmail.com>
2025-03-06 22:37:06 +0000

    interleave promotions and demotions in batches

--- linux/mm/nucleus_split_migrater.c
+++ linux/mm/nucleus_split_migrater.c
@@ -203,9 +203,6 @@ free_req:
 static void migrate_hugepages_and_basepages(unsigned int *promoted, unsigned int *demoted)
 {
     unsigned long flags;
-    LIST_HEAD(promotion_list);
-    LIST_HEAD(demotion_list);
-    struct lruvec *lruvecs[NUM_NUMA_NODES] = {NULL};
     struct nucleus_migrate_request *req, *req_tmp;
     struct nucleus_hugepage *hp;
     struct nucleus_basepage *bp;
@@ -215,148 +212,173 @@ static void migrate_hugepages_and_basepages(unsigned int *promoted, unsigned int
     pmd_t *pmd;
     pte_t *pte;
     unsigned long hp_addr, bp_addr;
-    unsigned int nr_taken[NUM_NUMA_NODES] = {0};
     int i, node_id, target_node;
+    unsigned int nr_to_promote, nr_promoted, total_promoted = 0, max_nr_to_promote = 2000;
+    unsigned int nr_to_demote, nr_demoted, total_demoted = 0, max_nr_to_demote = 2000;
+    bool is_empty;
 
     lru_add_drain();
 
     // pr_info("nucleus_split_migrater: locking migrate queue\n");
     spin_lock_irqsave(&nucleus_migrate_queue.request_queue_lock, flags);
     // pr_info("nucleus_split_migrater: locked migrate queue\n");
-    list_for_each_entry_safe(req, req_tmp, &nucleus_migrate_queue.request_queue, list) {
-        if (req->type == NUCLEUS_HUGEPAGE) {
-            hp = req->hp;
-            // pr_info("nucleus_split_migrater: migrate hp %lx\n", hp->address);
-        } else {
-            bp = req->bp;
-            hp = bp->hp;
-            // pr_info("nucleus_split_migrater: migrate hp %lx bp %u\n", hp->address, bp->offset);
-        }
-        hp_addr = hp->address << HPAGE_PMD_SHIFT;
-        if (!hp->mm) {
-            pr_info("nucleus_split_migrater: hp %lx mm not found\n", hp->address);
-            goto free_req;
-        }
-        mmap_read_lock(hp->mm);
-        // pr_info("nucleus_split_migrater: locked mm for hp %lx\n", hp->address);
-        pmd = mm_find_pmd(hp->mm, hp_addr);
-        if (!pmd) {
-            pr_info("nucleus_split_migrater: hp %lx pmd not found\n", hp->address);
-            mmap_read_unlock(hp->mm);
-            goto free_req;
-        }
-        if (pmd_trans_huge(*pmd)) {
-            page = pmd_page(*pmd);
-        } else {
-            bp_addr = hp_addr + (bp->offset << PAGE_SHIFT);
-            pte = pte_offset_map(pmd, bp_addr);
-            if (!pte) {
-                pr_info("nucleus_split_migrater: hp %lx bp %u pte not found\n", hp->address, bp->offset);
-                pte_unmap(pte);
+    do {
+        LIST_HEAD(promotion_list);
+        LIST_HEAD(demotion_list);
+        struct lruvec *lruvecs[NUM_NUMA_NODES] = {NULL};
+        unsigned int nr_taken[NUM_NUMA_NODES] = {0};
+        nr_to_promote = 0;
+        nr_to_demote = 0;
+        nr_promoted = 0;
+        nr_demoted = 0;
+
+        list_for_each_entry_safe(req, req_tmp, &nucleus_migrate_queue.request_queue, list) {
+            if (nr_to_promote >= max_nr_to_promote || nr_to_demote >= max_nr_to_demote) {
+                break;
+            }
+            if (req->type == NUCLEUS_HUGEPAGE) {
+                hp = req->hp;
+                // pr_info("nucleus_split_migrater: migrate hp %lx\n", hp->address);
+            } else {
+                bp = req->bp;
+                hp = bp->hp;
+                // pr_info("nucleus_split_migrater: migrate hp %lx bp %u\n", hp->address, bp->offset);
+            }
+            hp_addr = hp->address << HPAGE_PMD_SHIFT;
+            if (!hp->mm) {
+                pr_info("nucleus_split_migrater: hp %lx mm not found\n", hp->address);
+                goto free_req;
+            }
+            mmap_read_lock(hp->mm);
+            // pr_info("nucleus_split_migrater: locked mm for hp %lx\n", hp->address);
+            pmd = mm_find_pmd(hp->mm, hp_addr);
+            if (!pmd) {
+                pr_info("nucleus_split_migrater: hp %lx pmd not found\n", hp->address);
                 mmap_read_unlock(hp->mm);
                 goto free_req;
             }
-            page = pte_page(*pte);
-            pte_unmap(pte);
-            // pr_info("nucleus_split_migrater: hp %lx bp %u pte unmapped\n", hp->address, bp->offset);
-        }
-        if (!page) {
-            pr_info("nucleus_split_migrater: hp %lx page not found\n", hp->address);
+            if (pmd_trans_huge(*pmd)) {
+                page = pmd_page(*pmd);
+            } else {
+                bp_addr = hp_addr + (bp->offset << PAGE_SHIFT);
+                pte = pte_offset_map(pmd, bp_addr);
+                if (!pte) {
+                    pr_info("nucleus_split_migrater: hp %lx bp %u pte not found\n", hp->address, bp->offset);
+                    pte_unmap(pte);
+                    mmap_read_unlock(hp->mm);
+                    goto free_req;
+                }
+                page = pte_page(*pte);
+                pte_unmap(pte);
+                // pr_info("nucleus_split_migrater: hp %lx bp %u pte unmapped\n", hp->address, bp->offset);
+            }
+            if (!page) {
+                pr_info("nucleus_split_migrater: hp %lx page not found\n", hp->address);
+                mmap_read_unlock(hp->mm);
+                goto free_req;
+            }
+
+            // pr_info("nucleus_split_migrater: unlocking mm for hp %lx\n", hp->address);
             mmap_read_unlock(hp->mm);
-            goto free_req;
-        }
+            // pr_info("nucleus_split_migrater: unlocked mm for hp %lx\n", hp->address);
 
-        // pr_info("nucleus_split_migrater: unlocking mm for hp %lx\n", hp->address);
-        mmap_read_unlock(hp->mm);
-        // pr_info("nucleus_split_migrater: unlocked mm for hp %lx\n", hp->address);
+            lruvec = mem_cgroup_page_lruvec(page);
+            node_id = page_to_nid(page);
+            if (node_id >= NUM_NUMA_NODES) {
+                pr_info("nucleus_split_migrater: hp %lx invalid node id %d\n", hp->address, node_id);
+                goto free_req;
+            }
+            if (lruvecs[node_id] == NULL) {
+                lruvecs[node_id] = lruvec;
+            }
 
-        lruvec = mem_cgroup_page_lruvec(page);
-        node_id = page_to_nid(page);
-        if (node_id >= NUM_NUMA_NODES) {
-            pr_info("nucleus_split_migrater: hp %lx invalid node id %d\n", hp->address, node_id);
-            goto free_req;
-        }
-        if (lruvecs[node_id] == NULL) {
-            lruvecs[node_id] = lruvec;
-        }
+            // pr_info("nucleus_split_migrater: locking lruvec\n");
+            spin_lock_irq(&lruvec->lru_lock);
+            // pr_info("nucleus_split_migrater: locked lruvec, isolating page\n");
+            if (!__isolate_lru_page_prepare(page, 0)) {
+                spin_unlock_irq(&lruvec->lru_lock);
+                goto free_req;
+            }
 
-        // pr_info("nucleus_split_migrater: locking lruvec\n");
-        spin_lock_irq(&lruvec->lru_lock);
-        // pr_info("nucleus_split_migrater: locked lruvec, isolating page\n");
-        if (!__isolate_lru_page_prepare(page, 0)) {
+            // pr_info("nucleus_split_migrater: isolated page, getting page\n");
+            if (unlikely(!get_page_unless_zero(page))) {
+                spin_unlock_irq(&lruvec->lru_lock);
+                goto free_req;
+            }
+
+            // pr_info("nucleus_split_migrater: got page, test clear lru\n");
+            if (!TestClearPageLRU(page)) {
+                put_page(page);
+                spin_unlock_irq(&lruvec->lru_lock); 
+                goto free_req;
+            }
+
+            target_node = req->target_node;
+            if (target_node == HTMM_CXL_LOCAL_NUMA) {
+                list_move(&page->lru, &promotion_list);
+                nr_to_promote++;
+            } else {
+                list_move(&page->lru, &demotion_list);
+                nr_to_demote++;
+            }
+            // pr_info("nucleus_split_migrater: moved page to %d list\n", target_node);
+            update_lru_size(lruvec, page_lru(page), page_zonenum(page), -compound_nr(page));
+            // pr_info("nucleus_split_migrater: updated lru size\n");
             spin_unlock_irq(&lruvec->lru_lock);
-            goto free_req;
+            // pr_info("nucleus_split_migrater: unlocked lruvec\n");
+            nr_taken[node_id]++;
+
+    free_req:
+            atomic_dec(&hp->ref_count);
+            list_del(&req->list);
+            kfree(req);
         }
 
-        // pr_info("nucleus_split_migrater: isolated page, getting page\n");
-        if (unlikely(!get_page_unless_zero(page))) {
+        // pr_info("nucleus_split_migrater: mod node page state\n");
+        for (i = 0; i < NUM_NUMA_NODES; i++) {
+            if (lruvecs[i] && nr_taken[i] > 0) {
+                spin_lock_irq(&lruvecs[i]->lru_lock);
+                __mod_node_page_state(NODE_DATA(i), NR_ISOLATED_ANON, nr_taken[i]);
+                spin_unlock_irq(&lruvecs[i]->lru_lock);
+            }
+        }
+
+        pr_info("nucleus_split_migrater: demoting %u pages\n", nr_to_demote);
+        local_pgdat = NODE_DATA(HTMM_CXL_LOCAL_NUMA);
+        nr_demoted = migrate_page_list_safe(&demotion_list, local_pgdat, false);
+        pr_info("nucleus_split_migrater: demoted %u pages\n", nr_demoted);
+        total_demoted += nr_demoted;
+        if (!list_empty(&demotion_list)) {
+            // pr_info("nucleus_split_migrater: moving remaining demotion list to lruvec\n");
+            lruvec = lruvecs[HTMM_CXL_LOCAL_NUMA];
+            spin_lock_irq(&lruvec->lru_lock);
+            move_pages_to_lru(lruvec, &demotion_list);
+            __mod_node_page_state(local_pgdat, NR_ISOLATED_ANON, -nr_taken[HTMM_CXL_LOCAL_NUMA]);
             spin_unlock_irq(&lruvec->lru_lock);
-            goto free_req;
+            // pr_info("nucleus_split_migrater: moved remaining demotion list to lruvec\n");
         }
 
-        // pr_info("nucleus_split_migrater: got page, test clear lru\n");
-        if (!TestClearPageLRU(page)) {
-            put_page(page);
-            spin_unlock_irq(&lruvec->lru_lock); 
-            goto free_req;
+        pr_info("nucleus_split_migrater: promoting %u pages\n", nr_to_promote);
+        remote_pgdat = NODE_DATA(HTMM_CXL_REMOTE_NUMA);
+        nr_promoted = migrate_page_list_safe(&promotion_list, remote_pgdat, true);
+        pr_info("nucleus_split_migrater: promoted %u pages\n", nr_promoted);
+        total_promoted += nr_promoted;
+        if (!list_empty(&promotion_list)) {
+            // pr_info("nucleus_split_migrater: moving remaining promotion list to lruvec\n");
+            lruvec = lruvecs[HTMM_CXL_REMOTE_NUMA];
+            spin_lock_irq(&lruvec->lru_lock);
+            move_pages_to_lru(lruvec, &promotion_list);
+            __mod_node_page_state(remote_pgdat, NR_ISOLATED_ANON, -nr_taken[HTMM_CXL_REMOTE_NUMA]);
+            spin_unlock_irq(&lruvec->lru_lock);
+            // pr_info("nucleus_split_migrater: moved remaining promotion list to lruvec\n");
         }
 
-        target_node = req->target_node;
-        if (target_node == HTMM_CXL_LOCAL_NUMA) {
-            list_move(&page->lru, &promotion_list);
-        } else {
-            list_move(&page->lru, &demotion_list);
-        }
-        // pr_info("nucleus_split_migrater: moved page to %d list\n", target_node);
-        update_lru_size(lruvec, page_lru(page), page_zonenum(page), -compound_nr(page));
-        // pr_info("nucleus_split_migrater: updated lru size\n");
-        spin_unlock_irq(&lruvec->lru_lock);
-        // pr_info("nucleus_split_migrater: unlocked lruvec\n");
-        nr_taken[node_id]++;
-
-free_req:
-        atomic_dec(&hp->ref_count);
-        list_del(&req->list);
-        kfree(req);
-    }
-
-    // pr_info("nucleus_split_migrater: mod node page state\n");
-    for (i = 0; i < NUM_NUMA_NODES; i++) {
-        if (lruvecs[i] && nr_taken > 0) {
-            spin_lock_irq(&lruvecs[i]->lru_lock);
-            __mod_node_page_state(NODE_DATA(i), NR_ISOLATED_ANON, nr_taken[i]);
-            spin_unlock_irq(&lruvecs[i]->lru_lock);
-        }
-    }
-
-    // pr_info("nucleus_split_migrater: demoting pages\n");
-    local_pgdat = NODE_DATA(HTMM_CXL_LOCAL_NUMA);
-    *demoted = migrate_page_list_safe(&demotion_list, local_pgdat, false);
-    if (!list_empty(&demotion_list)) {
-        // pr_info("nucleus_split_migrater: moving remaining demotion list to lruvec\n");
-        lruvec = lruvecs[HTMM_CXL_LOCAL_NUMA];
-        spin_lock_irq(&lruvec->lru_lock);
-		move_pages_to_lru(lruvec, &demotion_list);
-		__mod_node_page_state(local_pgdat, NR_ISOLATED_ANON, -nr_taken[HTMM_CXL_LOCAL_NUMA]);
-		spin_unlock_irq(&lruvec->lru_lock);
-        // pr_info("nucleus_split_migrater: moved remaining demotion list to lruvec\n");
-    }
-
-    // pr_info("nucleus_split_migrater: promoting pages\n");
-    remote_pgdat = NODE_DATA(HTMM_CXL_REMOTE_NUMA);
-    *promoted = migrate_page_list_safe(&promotion_list, remote_pgdat, true);
-    if (!list_empty(&promotion_list)) {
-        // pr_info("nucleus_split_migrater: moving remaining promotion list to lruvec\n");
-        lruvec = lruvecs[HTMM_CXL_REMOTE_NUMA];
-        spin_lock_irq(&lruvec->lru_lock);
-        move_pages_to_lru(lruvec, &promotion_list);
-        __mod_node_page_state(remote_pgdat, NR_ISOLATED_ANON, -nr_taken[HTMM_CXL_REMOTE_NUMA]);
-        spin_unlock_irq(&lruvec->lru_lock);
-        // pr_info("nucleus_split_migrater: moved remaining promotion list to lruvec\n");
-    }
-
+        is_empty = list_empty(&nucleus_migrate_queue.request_queue);
+    } while (!is_empty);
     // pr_info("nucleus_split_migrater: unlocking migrate queue\n");
     spin_unlock_irqrestore(&nucleus_migrate_queue.request_queue_lock, flags);
+    *promoted = total_promoted;
+    *demoted = total_demoted;
     // pr_info("nucleus_split_migrater: unlocked migrate queue\n");
 }
 
