99d013f1952641b647eb9167470eb89e4857e2cb
Omar Eqbal <eqbalomar@gmail.com>
2025-03-09 03:02:47 +0000

    move nucleus_algorithm tracepoint to separate file as kernel module will create this tracepoint

--- linux/include/trace/events/nucleus.h
+++ linux/include/trace/events/nucleus.h
@@ -93,26 +93,6 @@ TRACE_EVENT(nucleus_migrate,
    TP_printk("[nucleus] nr_promoted=%lu, nr_demoted=%lu, time_ms=%lu", __entry->nr_promoted, __entry->nr_demoted, __entry->time_ms)
 );
 
-TRACE_EVENT(nucleus_algorithm,
-   TP_PROTO(unsigned long to_split, unsigned long to_merge, unsigned long to_promote, unsigned long to_demote, unsigned long time_ms),
-   TP_ARGS(to_split, to_merge, to_promote, to_demote, time_ms),
-   TP_STRUCT__entry(
-      __field(unsigned long, to_split)
-      __field(unsigned long, to_merge)
-      __field(unsigned long, to_promote)
-      __field(unsigned long, to_demote)
-      __field(unsigned long, time_ms)
-   ),
-   TP_fast_assign(
-      __entry->to_split = to_split;
-      __entry->to_merge = to_merge;
-      __entry->to_promote = to_promote;
-      __entry->to_demote = to_demote;
-      __entry->time_ms = time_ms;
-   ),
-   TP_printk("[nucleus] to_split=%lu, to_merge=%lu, to_promote=%lu, to_demote=%lu, time_ms=%lu", __entry->to_split, __entry->to_merge, __entry->to_promote, __entry->to_demote, __entry->time_ms)
-);
-
 #endif /* _TRACE_NUCLEUS_H */
 
 /* This part must be outside protection */
