641d58d40bdd7a5feda8970b1ef5485067b55cbd
Omar Eqbal <eqbalomar@gmail.com>
2025-03-11 00:56:32 +0000

    use 128GB wss and 16 threads

--- memtis-userspace/bench_cmds/gups-x0-access-r.sh
+++ memtis-userspace/bench_cmds/gups-x0-access-r.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 BIN=/home/omar/nucleus/apps
-gups_cores=1
+gups_cores=16
 duration=1000
-wss=16
+wss=128
 
-BENCH_DRAM="1GB"
+BENCH_DRAM="64GB"
 
 BENCH_RUN="${BIN}/gups-x0-access-r ${gups_cores} ${duration} ${wss}"
 
