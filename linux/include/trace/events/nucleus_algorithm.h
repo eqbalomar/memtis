a19c57682f4ffeccd42b03ed589c50cc761a28dc
Omar Eqbal <eqbalomar@gmail.com>
2025-03-12 03:35:25 +0000

    add time breakdown in algo tracepoint

--- linux/include/trace/events/nucleus_algorithm.h
+++ linux/include/trace/events/nucleus_algorithm.h
@@ -8,23 +8,38 @@
 #include <linux/tracepoint.h>
 
 TRACE_EVENT(nucleus_algorithm,
-   TP_PROTO(unsigned long to_split, unsigned long to_merge, unsigned long to_promote, unsigned long to_demote, unsigned long time_ms),
-   TP_ARGS(to_split, to_merge, to_promote, to_demote, time_ms),
+   TP_PROTO(unsigned long to_split, unsigned long to_merge, unsigned long to_promote, unsigned long to_demote,
+      unsigned long init_time_ms, unsigned long pack_bp_time_ms, unsigned long sort_hp_time_ms, unsigned long process_hp_time_ms, unsigned long enqueue_req_time_ms, unsigned long cleanup_time_ms, unsigned long total_time_ms),
+   TP_ARGS(to_split, to_merge, to_promote, to_demote, init_time_ms, pack_bp_time_ms, sort_hp_time_ms, process_hp_time_ms, enqueue_req_time_ms, cleanup_time_ms, total_time_ms),
    TP_STRUCT__entry(
       __field(unsigned long, to_split)
       __field(unsigned long, to_merge)
       __field(unsigned long, to_promote)
       __field(unsigned long, to_demote)
-      __field(unsigned long, time_ms)
+      __field(unsigned long, init_time_ms)
+      __field(unsigned long, pack_bp_time_ms)
+      __field(unsigned long, sort_hp_time_ms)
+      __field(unsigned long, process_hp_time_ms)
+      __field(unsigned long, enqueue_req_time_ms)
+      __field(unsigned long, cleanup_time_ms)
+      __field(unsigned long, total_time_ms)
    ),
    TP_fast_assign(
       __entry->to_split = to_split;
       __entry->to_merge = to_merge;
       __entry->to_promote = to_promote;
       __entry->to_demote = to_demote;
-      __entry->time_ms = time_ms;
+      __entry->init_time_ms = init_time_ms;
+      __entry->pack_bp_time_ms = pack_bp_time_ms;
+      __entry->sort_hp_time_ms = sort_hp_time_ms;
+      __entry->process_hp_time_ms = process_hp_time_ms;
+      __entry->enqueue_req_time_ms = enqueue_req_time_ms;
+      __entry->cleanup_time_ms = cleanup_time_ms;
+      __entry->total_time_ms = total_time_ms;
    ),
-   TP_printk("[nucleus_algorithm] to_split=%lu, to_merge=%lu, to_promote=%lu, to_demote=%lu, time_ms=%lu", __entry->to_split, __entry->to_merge, __entry->to_promote, __entry->to_demote, __entry->time_ms)
+   TP_printk("[nucleus_algorithm] to_split=%lu, to_merge=%lu, to_promote=%lu, to_demote=%lu, init_time_ms=%lu, pack_bp_time_ms=%lu, sort_hp_time_ms=%lu, process_hp_time_ms=%lu, enqueue_req_time_ms=%lu, cleanup_time_ms=%lu, total_time_ms=%lu",
+      __entry->to_split, __entry->to_merge, __entry->to_promote, __entry->to_demote,
+      __entry->init_time_ms, __entry->pack_bp_time_ms, __entry->sort_hp_time_ms, __entry->process_hp_time_ms, __entry->enqueue_req_time_ms, __entry->cleanup_time_ms, __entry->total_time_ms)
 );
 
 #endif /* _TRACE_NUCLEUS_ALGORITHM_H */
