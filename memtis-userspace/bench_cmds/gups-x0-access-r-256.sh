fdc2cce0866eb25a7801d5932045aa6eca6ac1c8
Omar Eqbal <eqbalomar@gmail.com>
2025-03-13 22:00:36 +0000

    add scripts for different workloads

--- memtis-userspace/bench_cmds/gups-x0-access-r-256.sh
+++ memtis-userspace/bench_cmds/gups-x0-access-r-256.sh
@@ -4,10 +4,11 @@ BIN=/home/omar/nucleus/apps
 gups_cores=16
 duration=1000
 wss=128
+num_bps_accessed=256
 
 BENCH_DRAM="64GB"
 
-BENCH_RUN="${BIN}/gups-x0-access-r ${gups_cores} ${duration} ${wss}"
+BENCH_RUN="${BIN}/gups-x0-access-r ${gups_cores} ${duration} ${wss} ${num_bps_accessed}"
 
 export BENCH_RUN
 export BENCH_DRAM
