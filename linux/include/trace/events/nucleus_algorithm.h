eb7e9afdf82d28ce9cf06f19b29e3008dd2aca62
Omar Eqbal <eqbalomar@gmail.com>
2025-03-13 02:54:00 +0000

    log local and remote loads in tracepoint

--- linux/include/trace/events/nucleus_algorithm.h
+++ linux/include/trace/events/nucleus_algorithm.h
@@ -8,10 +8,12 @@
 #include <linux/tracepoint.h>
 
 TRACE_EVENT(nucleus_algorithm,
-   TP_PROTO(unsigned long to_split, unsigned long to_merge, unsigned long to_promote, unsigned long to_demote,
-      unsigned long init_time_ms, unsigned long pack_bp_time_ms, unsigned long sort_hp_time_ms, unsigned long process_hp_time_ms, unsigned long enqueue_req_time_ms, unsigned long cleanup_time_ms, unsigned long total_time_ms),
-   TP_ARGS(to_split, to_merge, to_promote, to_demote, init_time_ms, pack_bp_time_ms, sort_hp_time_ms, process_hp_time_ms, enqueue_req_time_ms, cleanup_time_ms, total_time_ms),
+   TP_PROTO(unsigned long loads_local, unsigned long loads_remote, unsigned long to_split, unsigned long to_merge, unsigned long to_promote, unsigned long to_demote,
+      unsigned long init_time_ms, unsigned long pack_bp_time_ms, unsigned long sort_hp_time_ms, unsigned long process_hp_time_ms, unsigned long enqueue_req_time_ms, unsigned long cleanup_time_ms),
+   TP_ARGS(loads_local, loads_remote, to_split, to_merge, to_promote, to_demote, init_time_ms, pack_bp_time_ms, sort_hp_time_ms, process_hp_time_ms, enqueue_req_time_ms, cleanup_time_ms),
    TP_STRUCT__entry(
+      __field(unsigned long, loads_local)
+      __field(unsigned long, loads_remote)
       __field(unsigned long, to_split)
       __field(unsigned long, to_merge)
       __field(unsigned long, to_promote)
@@ -22,9 +24,10 @@ TRACE_EVENT(nucleus_algorithm,
       __field(unsigned long, process_hp_time_ms)
       __field(unsigned long, enqueue_req_time_ms)
       __field(unsigned long, cleanup_time_ms)
-      __field(unsigned long, total_time_ms)
    ),
    TP_fast_assign(
+      __entry->loads_local = loads_local;
+      __entry->loads_remote = loads_remote;
       __entry->to_split = to_split;
       __entry->to_merge = to_merge;
       __entry->to_promote = to_promote;
@@ -35,11 +38,10 @@ TRACE_EVENT(nucleus_algorithm,
       __entry->process_hp_time_ms = process_hp_time_ms;
       __entry->enqueue_req_time_ms = enqueue_req_time_ms;
       __entry->cleanup_time_ms = cleanup_time_ms;
-      __entry->total_time_ms = total_time_ms;
    ),
-   TP_printk("[nucleus_algorithm] to_split=%lu, to_merge=%lu, to_promote=%lu, to_demote=%lu, init_time_ms=%lu, pack_bp_time_ms=%lu, sort_hp_time_ms=%lu, process_hp_time_ms=%lu, enqueue_req_time_ms=%lu, cleanup_time_ms=%lu, total_time_ms=%lu",
-      __entry->to_split, __entry->to_merge, __entry->to_promote, __entry->to_demote,
-      __entry->init_time_ms, __entry->pack_bp_time_ms, __entry->sort_hp_time_ms, __entry->process_hp_time_ms, __entry->enqueue_req_time_ms, __entry->cleanup_time_ms, __entry->total_time_ms)
+   TP_printk("[nucleus_algorithm] loads_local=%lu, loads_remote=%lu, to_split=%lu, to_merge=%lu, to_promote=%lu, to_demote=%lu, init_time_ms=%lu, pack_bp_time_ms=%lu, sort_hp_time_ms=%lu, process_hp_time_ms=%lu, enqueue_req_time_ms=%lu, cleanup_time_ms=%lu",
+      __entry->loads_local, __entry->loads_remote, __entry->to_split, __entry->to_merge, __entry->to_promote, __entry->to_demote,
+      __entry->init_time_ms, __entry->pack_bp_time_ms, __entry->sort_hp_time_ms, __entry->process_hp_time_ms, __entry->enqueue_req_time_ms, __entry->cleanup_time_ms)
 );
 
 #endif /* _TRACE_NUCLEUS_ALGORITHM_H */
