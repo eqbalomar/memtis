30ca3f90aca7da3f294c6acf1de74ef4fa4ce7a2
Omar Eqbal <eqbalomar@gmail.com>
2025-03-17 20:56:56 +0000

    log sample period, enable cooling and stores counter

--- linux/mm/htmm_sampler.c
+++ linux/mm/htmm_sampler.c
@@ -18,6 +18,9 @@ struct task_struct *access_sampling = NULL;
 struct perf_event ***mem_event = NULL;
 int *cpus_in_socket = NULL;
 
+unsigned long current_sample_period = 0;
+EXPORT_SYMBOL(current_sample_period);
+
 static bool valid_va(unsigned long addr)
 {
     if (!(addr >> (PGDIR_SHIFT + 9)) && addr != 0)
@@ -36,8 +39,8 @@ static __u64 get_pebs_event(enum events e)
 		return NVM_LLC_LOAD_MISS;
 	    else
 		return N_HTMMEVENTS;
-	// case MEMWRITE:
-	//     return ALL_STORES;
+	case MEMWRITE:
+	    return ALL_STORES;
 	case CXLREAD:
 	    if (htmm_cxl_mode)
 		return REMOTE_DRAM_LLC_LOAD_MISS;
@@ -215,6 +218,7 @@ static int ksamplingd(void *data)
     /* used for periodic checks*/
     unsigned long cpucap_period = msecs_to_jiffies(15000); // 15s
     unsigned long sample_period = 0;
+	WRITE_ONCE(current_sample_period, get_sample_period(sample_period));
     unsigned long sample_inst_period = 0;
     /* report cpu/period stat */
     unsigned long trace_cputime, trace_period = msecs_to_jiffies(1500); // 3s
@@ -368,15 +372,19 @@ static int ksamplingd(void *data)
 		/* only increase by 1 */
 		unsigned long tmp1 = sample_period, tmp2 = sample_inst_period;
 		increase_sample_period(&sample_period, &sample_inst_period);
-		if (tmp1 != sample_period || tmp2 != sample_inst_period)
+		if (tmp1 != sample_period || tmp2 != sample_inst_period) {
 		    pebs_update_period(get_sample_period(sample_period),
 				       get_sample_inst_period(sample_inst_period));
+			WRITE_ONCE(current_sample_period, get_sample_period(sample_period));
+		}
 	    } else if (cputime < (ksampled_soft_cpu_quota - 5) && sample_period) {
 		unsigned long tmp1 = sample_period, tmp2 = sample_inst_period;
 		decrease_sample_period(&sample_period, &sample_inst_period);
-		if (tmp1 != sample_period || tmp2 != sample_inst_period)
+		if (tmp1 != sample_period || tmp2 != sample_inst_period) {
 		    pebs_update_period(get_sample_period(sample_period),
 				    get_sample_inst_period(sample_inst_period));
+			WRITE_ONCE(current_sample_period, get_sample_period(sample_period));
+		}
 	    }
 	    /* does it need to prevent ping-pong behavior? */
 	    
