02d0e9d989a0bfd8e14050681e549e8ae2883aa1
Omar Eqbal <eqbalomar@gmail.com>
2026-03-03 19:18:00 +0000

    add scripts for experiments

--- memtis-userspace/bench_cmds/gups-x0-access-diff-skew-r.sh
+++ memtis-userspace/bench_cmds/gups-x0-access-diff-skew-r.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-BIN=/home/omar/nucleus/apps
+BIN=/home/omar/nucleus/apps_new
 gups_cores=16
 duration=1200
-wss=128
+wss=64
 
-BENCH_DRAM="64GB"
+BENCH_DRAM="32GB"
 
 BENCH_RUN="${BIN}/gups-x0-access-diff-skew-r ${gups_cores} ${duration} ${wss}"
 
--- memtis-userspace/bench_cmds/gups-x0-access-r-256.sh
+++ memtis-userspace/bench_cmds/gups-x0-access-r-256.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-BIN=/home/omar/nucleus/apps
+BIN=/home/omar/nucleus/apps_new
 gups_cores=16
 duration=1200
-wss=128
+wss=64
 num_bps_accessed=256
 
-BENCH_DRAM="64GB"
+BENCH_DRAM="32GB"
 
 BENCH_RUN="${BIN}/gups-x0-access-r ${gups_cores} ${duration} ${wss} ${num_bps_accessed}"
 
