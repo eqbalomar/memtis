faa0056239b61030c0abe9ffdb0d5c9c17e6f39a
Omar Eqbal <eqbalomar@gmail.com>
2026-03-12 15:45:23 +0000

    add scripts

---
+++ memtis-userspace/bench_cmds/gups-x0-access-r-8-sweep-hp1000.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BIN=/home/omar/nucleus/apps_new
+gups_cores=16
+duration=300
+wss=64
+num_bps_accessed=8
+hp_frac=1000
+
+BENCH_DRAM="32GB"
+
+BENCH_RUN="${BIN}/gups-x0-access-r-sweep ${gups_cores} ${duration} ${wss} ${num_bps_accessed} ${hp_frac}"
+
+export BENCH_RUN
+export BENCH_DRAM
