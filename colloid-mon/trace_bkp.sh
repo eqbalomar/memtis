fe35214a855e988fffe5f01bfbbaaa0edae42d09
Omar Eqbal <eqbalomar@gmail.com>
2026-03-13 01:47:09 +0000

    update bpf trace for translation latency measurement

--- colloid-mon/trace_bkp.sh
+++ colloid-mon/trace_bkp.sh
@@ -11,12 +11,12 @@ addr_inserts_remote=$(cat /proc/kallsyms | grep smoothed_inserts_remote | awk '{
 addr_walk_completed=$(cat /proc/kallsyms | grep smoothed_walk_completed | grep -v _hp | grep -v _bp | awk '{print "0x"$1}')
 # addr_walk_completed_hp=$(cat /proc/kallsyms | grep smoothed_walk_completed_hp | awk '{print "0x"$1}')
 # addr_walk_completed_bp=$(cat /proc/kallsyms | grep smoothed_walk_completed_bp | awk '{print "0x"$1}')
-addr_llc_misses=$(cat /proc/kallsyms | grep smoothed_llc_misses | grep nucleus_algorithm | awk '{print "0x"$1}')
-addr_local_llc_misses=$(cat /proc/kallsyms | grep smoothed_local_llc_misses | grep nucleus_algorithm | awk '{print "0x"$1}')
-addr_remote_llc_misses=$(cat /proc/kallsyms | grep smoothed_remote_llc_misses | grep nucleus_algorithm | awk '{print "0x"$1}')
-addr_llc_hits=$(cat /proc/kallsyms | grep smoothed_llc_hits | grep nucleus_algorithm | awk '{print "0x"$1}')
-addr_local_llc_hits=$(cat /proc/kallsyms | grep smoothed_local_llc_hits | grep nucleus_algorithm | awk '{print "0x"$1}')
-addr_remote_llc_hits=$(cat /proc/kallsyms | grep smoothed_remote_llc_hits | grep nucleus_algorithm | awk '{print "0x"$1}')
+addr_llc_misses=$(cat /proc/kallsyms | grep smoothed_llc_misses | grep nucleus_mon | awk '{print "0x"$1}')
+addr_local_llc_misses=$(cat /proc/kallsyms | grep smoothed_local_llc_misses | grep nucleus_mon | awk '{print "0x"$1}')
+addr_remote_llc_misses=$(cat /proc/kallsyms | grep smoothed_remote_llc_misses | grep nucleus_mon | awk '{print "0x"$1}')
+addr_llc_hits=$(cat /proc/kallsyms | grep smoothed_llc_hits | grep nucleus_mon | awk '{print "0x"$1}')
+addr_local_llc_hits=$(cat /proc/kallsyms | grep smoothed_local_llc_hits | grep nucleus_mon | awk '{print "0x"$1}')
+addr_remote_llc_hits=$(cat /proc/kallsyms | grep smoothed_remote_llc_hits | grep nucleus_mon | awk '{print "0x"$1}')
 
 # bpftrace -e "BEGIN {@start = nsecs;} interval:s:1 {printf(\"%ld, colloid_local_lat_gt_remote: %d, local_occ: %lu, remote_occ: %lu, local_inserts: %lu, remote_inserts: %lu, p_lo: %lu, p_hi: %lu, delta_p=%lu, dynlimit=%lu\n\", (nsecs-@start)/1e9, *kaddr(\"colloid_local_lat_gt_remote\"), *($addr_occ_local), *($addr_occ_remote), *($addr_inserts_local), *($addr_inserts_remote), *($addr_p_lo), *($addr_p_hi), *kaddr(\"colloid_delta_p\"), *kaddr(\"colloid_dynlimit\"));}"
 bpftrace -e "BEGIN {@start = nsecs;} interval:s:1 {
