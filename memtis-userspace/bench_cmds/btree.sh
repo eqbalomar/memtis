86ee7268ce35710697280e74a669e4c8035a800d
Omar Eqbal <eqbalomar@gmail.com>
2026-03-14 16:54:39 +0000

    update scripts

--- memtis-userspace/bench_cmds/btree.sh
+++ memtis-userspace/bench_cmds/btree.sh
@@ -2,7 +2,7 @@
 
 BIN=/home/omar/vmitosis-workloads/bin
 BENCH_RUN="taskset -c 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31 ${BIN}/bench_btree_mt"
-BENCH_DRAM="6GB"
+BENCH_DRAM="4500MB"
 # NVM_RATIO="1:2"
 
 # if [[ "x${NVM_RATIO}" == "x1:16" ]]; then
--- memtis-userspace/bench_cmds/cachelib.sh
+++ memtis-userspace/bench_cmds/cachelib.sh
@@ -3,7 +3,7 @@
 BIN=/home/omar/CacheLib/opt/cachelib/bin
 WORKLOAD=/home/omar/colloid/workloads
 BENCH_RUN="taskset -c 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31 ${BIN}/cachebench --json_test_config ${WORKLOAD}/cachelib/kvcache_reg/config2.json --progress 1"
-BENCH_DRAM="18000MB"
+BENCH_DRAM="6GB"
 
 # if [[ "x${NVM_RATIO}" == "x1:32" ]]; then
 #     BENCH_DRAM="1803MB"
